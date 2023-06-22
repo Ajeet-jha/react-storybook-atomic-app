cf4678cd7c55d8d4601cb42ee0c70001b5f5438b
Ajeet-jha <jhaajeetcs@gmail.com>
2023-06-22 19:20:35 +0530

    install husky.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      Test app
-    </div>
-  );
+  return <div className="App">Test app</div>;
 }
 
 export default App;
